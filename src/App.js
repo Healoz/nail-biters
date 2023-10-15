4c68fea1ca7d4053bab3000fd16bc6f53eccbb22
Healoz <lauren.easter59@gmail.com>
2023-10-16 09:22:31 +1100

    Creating shaking text animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './ShakeAnimation.css';
+import ShakingText from './Components/ShakingText';
+import React, { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <ShakingText text="hello" />
+      <ShakingText text="howdy stranger how are you?" />
     </div>
   );
 }
