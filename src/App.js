94098349719bfcc7a6fc9e92e97d0b6e4e14884b
Healoz <lauren.easter59@gmail.com>
2023-10-16 12:50:12 +1100

    created basic layout of player

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import './ShakeAnimation.css';
 import ShakingText from './Components/ShakingText';
 import MoveSelection from './Components/MoveSelection';
+import GameScene from './Components/GameScene';
 import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [player, setPlayer] = React.useState("player1")
+  const [enemy, setEnemy] = React.useState("enemy")
+
   return (
     <div className="App">
       <main>
-        <section className='game-scene'></section>
+        <GameScene />
         <MoveSelection />
       </main>
       
